51e3f1e4113a2324b79f73dbc4c9862441bbe6dd
fg98ff@gmail.com <81941690+FG98F@users.noreply.github.com>
2021-04-30 23:20:57 -0400

    actualizar

---
+++ src/asw.js
@@ -0,0 +1,9 @@
+const bottt = (prefix) => {
+return `
+*Bot Sedang Perbaikan/Di Private Oleh Owner Karena Ada Kendala*\n\n *Mohon Pengertiannya*
+Sekali Lagi Maaf
+
+*Silahkan Chat Owner Untuk Mempublic Bot Dengan Cara ${prefix}bott aktif/nonaktif* \n\nOwner : wa.me/6285343788098
+`
+}
+exports.bottt = bottt
